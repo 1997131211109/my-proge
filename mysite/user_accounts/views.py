74af1b1a54de4c92f86b0c23939e2fe62ea1d252
denys <petrenckoden215@gmail.com>
2023-06-17 18:16:28 +0300

    5d . part  + profile (base)

--- mysite/user_accounts/views.py
+++ mysite/user_accounts/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from .forms import UserProfileForm
+from .forms import UserProfileForm, PostForm, CommentForm
+from blog.models import Post, Comment
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'create_post.html', {'form': form})
+
+def create_comment(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', slug=post.slug)
+    else:
+        form = CommentForm()
+    return render(request, 'create_comment.html', {'form': form})
 
 @login_required
 def profile(request):
@@ -12,14 +38,12 @@ def profile(request):
         form = UserProfileForm(request.POST, instance=request.user.profile)
         if form.is_valid():
             form.save()
-            return redirect('home')  # Перенаправлення на головну сторінку
+            return redirect('home')
     else:
         form = UserProfileForm(instance=request.user.profile)
     return render(request, 'profile.html', {'form': form})
-
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
-# Create your views here.
