ac42209bc4d5518d6aa499c2d1c427ed73acf798
denys <petrenckoden215@gmail.com>
2023-06-17 11:37:11 +0300

    5d . part  + profile

--- mysite/user_accounts/models.py
+++ mysite/user_accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=500, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
 
 # Create your models here.
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- mysite/user_accounts/views.py
+++ mysite/user_accounts/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import UserProfileForm
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('home')  # Перенаправлення на головну сторінку
+    else:
+        form = UserProfileForm(instance=request.user.profile)
+    return render(request, 'profile.html', {'form': form})
 
 
 class SignUpView(generic.CreateView):
